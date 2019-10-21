50753a486e7b06d76b01780656e7de5e55114fe8
jairoalzate98 <jairo.alzate@uptc.edu.co>
2019-10-21 08:41:24 -0500

    Se agrego reporte I05

--- BackendProyecto/routes/investment5.js
+++ BackendProyecto/routes/investment5.js
@@ -41,4 +41,68 @@ router.post('/add', (req, res) => {
     res.send('ok');
 });
 
+router.post('/getData', (req, res) => {
+    sendData(req, res);
+});
+
+async function sendData(req, res){
+    var faculty = req.body.facultyId;
+    var facu = getFaculty(faculty);
+    var years = [2014, 2015, 2016, 2017, 2018];
+    let data = [];
+    let value = 0;
+    years.forEach(function(element) {
+        investment5.find({"Facultad": facu, Vigenciadocumento: element}, function (err, inv) {
+            if(err){
+                return res.status(500).send("Error al realizar la peticion");
+            } 
+            if(!inv) {
+                return res.status(404).send("No hay datos");
+            }
+            for(var i = 0; i < inv.length; i++){
+                value += inv[i].Valorrecaudo;
+            }
+            data.push({'Anio': element,  'Total': value});
+            value = 0;
+        });
+    });
+    await resolveAfter10Seconds(10);
+    res.send(data);
+}
+
+function getFaculty(fac){
+    switch(fac){
+        case 1:
+            return "Ciencias";
+        case 2:
+            return "Ciencias Agropecuarias";
+        case 3:
+            return "Ciencias de la Educación";
+        case 4:
+            return "CIENCIAS DE LA SALUD";
+        case 5:
+            return "Ciencias Economicas y administrativas";
+        case 6:
+            return "Derecho y Ciencias Sociales";
+        case 7:
+            return "Estudios a Distancia";
+        case 8:
+            return "INGENIERIA";
+        case 9:
+            return "Chiquinquirá";
+        case 10:
+            return "Duitama";
+        case 11:
+            return "Sogamoso";
+    }
+}
+
+function resolveAfter10Seconds(x) { 
+    return new Promise(resolve => {
+        setTimeout(() => {
+        resolve(x);
+    }, 6000);
+    });
+}
+
 module.exports = router;
